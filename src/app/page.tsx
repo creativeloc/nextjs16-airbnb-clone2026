3f375817a3ee615f80934784ec112964a30aa723
Lawrence Caulker <locaulker@gmail.com>
2026-07-09 06:02:19 -0400

    implement global layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-black text-2xl">Airbnb Clone</h1>
+    <div className="flex flex-col flex-1 items-center justify-center bg-black font-sans dark:bg-black">
+      <h1 className="text-white text-2xl">Airbnb Clone</h1>
     </div>
   )
 }
